9f3477be268ea071b60d37ad6fc412142421e808
Angelina Tsuboi <>
2021-05-10 01:24:54 +0000

    Question 4 2018

--- 2018_Solutions/Questions.java
+++ 2018_Solutions/Questions.java
@@ -82,3 +82,33 @@
     }
 
   }
+
+// Question 4: A
+public static int[] getColumn(int[][] arr2D, int c){
+  int[] result = new int[arr2D.length];
+  for(int i = 0; i < arr2D.length; i++){
+    result[i] = arr2D[i][c];
+  }
+  return result;
+}
+
+// Question 4: B
+public static boolean isLatin(int[][] square){
+  if(containsDuplicates(square[0])){
+      return false;
+  }
+
+  for(int r = 1; r < square.length; r++){
+    if(!hasAllValues(square[0], square[r])){
+      return false;
+    }
+  }
+
+  for(int c = 0; c < square[0].length; c++){
+    if(!hasAllValues(square[0], getColumn(square, c))){
+      return false;
+    }
+  }
+  
+  return true;
+}
