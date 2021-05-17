fa1a6b66c38a7fb1b18331316940ac712b197be7
Angelina Tsuboi <>
2021-05-17 19:06:01 +0000

    Question 2 2016

--- 2016_Solutions/Questions.java
+++ 2016_Solutions/Questions.java
@@ -26,3 +26,27 @@ public RandomLetterChooser(String str){
   super(getSingleLetters(str));
 }
 
+// Question 2: A
+public LogMessage(String message){
+  int colonIndex = message.indexOf(":");
+  machineId = message.substring(0,colonIndex);
+  description = message.substring(colonIndex + 1);
+}
+
+// Question 2: B
+public boolean containsWord(String keyword){
+  if(description.equals(keyword)){
+    return true;
+  }else if(description.indexOf(keyword + " ") == 0){
+    return true;
+  }else if(description.indexOf(" " + keyword + " ") != -1){
+    return true;
+  }else if(description.length() > keyword.length()){
+    if(description.substring(keyword.length() - keyword.length() - 1).equals(" " + keyword)){
+      return true;
+    }
+  }
+  return false;
+}
+
+
